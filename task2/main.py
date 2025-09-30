92de2174fe8077d55cfe5bfb24ab384f746dd4f4
Janakiin <59832231+Janakiin@users.noreply.github.com>
2025-09-30 17:19:14 +0200

    Update main.py

--- task2/main.py
+++ task2/main.py
@@ -1,22 +1,25 @@
-from animalActionsPresenter import AnimalActionsPresenter
+import random
+from animalPresenter import AnimalPresenter
 from herbivore import Herbivore
-from predator import Predator
+from infoFetcher import InfoFetcher
+from carnivore import Carnivore
 
 def main():
     animals = [
-        Predator("Leonidas der Löwe", 5, 190.0, "Antilopen", 24),
-        Herbivore("Eleas der Elefant", 12, 5400.0, "Akazienblätter", 0),
-        Herbivore("Beros der Bulle", 3, 123.3, "Luzerne", 1)
+        Carnivore("Leonidas", "Löwe", 5, 190.0, "Antilopen", 24, False),
+        Carnivore("", "", False, False, "", False, False),
+        Herbivore("Eleas","Elefant", 1, 5400.0, "Akazienblätter", False, True, False),
+        Herbivore("Beros", "Bulle", 3, 123.3, "Luzerne", True, True, True)
     ]
 
-    printer = AnimalActionsPresenter()
+    fetcher = InfoFetcher()
+    presenter = AnimalPresenter()
 
     for animal in animals:
-        printer.present(animal.describe())
-        if isinstance(animal, Predator):
-            printer.present(animal.hunt(5.0))
-        elif isinstance(animal, Herbivore):
-            printer.present(animal.graze(50.0))
+        if animal.is_valid_animal:
+            foodAmount = random.uniform(0.5, 10.3)
+            animalInfos = fetcher.fetch(animal, foodAmount)
+            presenter.present(animalInfos)
 
 if __name__ == "__main__":
     main()
