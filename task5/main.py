295dd610bfa8c8915b0a17e11ae0df8ef7ea35b0
Janakiin <59832231+Janakiin@users.noreply.github.com>
2025-10-01 11:36:53 +0200

    Create main.py

---
+++ task5/main.py
@@ -0,0 +1,9 @@
+from function import factorial
+
+def main():
+    number = 4
+    result = factorial(number)
+    print(f"The factorial of {number} is {result}")
+
+if __name__ == "__main__":
+    main()
